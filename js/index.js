c5ffe8cf57b897a95cd711343cbc3656aada50d9
topiniu <topiniu@outlook.com>
2017-07-10 20:17:22 +0800

    branch test

--- js/index.js
+++ js/index.js
@@ -2,10 +2,10 @@
  * Created by topiniu on 2017/7/3.
  */
 var localData = [];
-$(function(){
-    $("#j_checkBox").change(function(){
+$(function () {
+    $("#j_checkBox").change(function () {
         var file = $("#j_checkBox").val();
-        if(!v(file)){
+        if (!v(file)) {
             alert("Must be jpg or jpeg or png file");
             $("#j_checkBox").val(null);
             return;
@@ -27,85 +27,80 @@ $(function(){
             contentType: false,
             cache: false,
             timeout: 600000,
-            success: function(data){
+            success: function (data) {
                 loadData(JSON.parse(data));
                 saveData(JSON.parse(data));
 
                 $(".j_loadingCover").fadeOut("slow");
             },
-            error: function(e){
+            error: function (e) {
                 alert("Something wrong");
             }
         });
     });
 
-    $(".j_path").on("click",function(){
+    $(".j_path").on("click", function () {
         // alert($(this));
         $(this).select()
     });
 
-    new Clipboard('.btn',{
-        target: function(trigger){
+    new Clipboard('.btn', {
+        target: function (trigger) {
             return trigger;
         }
     });
 
     // new Clipboard('.btn');
-    $("#j_checkBox").on("mouseenter",function(){
-        $(".j_inputBox").css("backgroundColor","#BDC0BA")
+    $("#j_checkBox").on("mouseenter", function () {
+        $(".j_inputBox").css("backgroundColor", "#BDC0BA")
         // alert(0);
     });
-    $("#j_checkBox").on("mouseleave",function(){
-        $(".j_inputBox").css("backgroundColor","rgb(145,152,159)")
+    $("#j_checkBox").on("mouseleave", function () {
+        $(".j_inputBox").css("backgroundColor", "rgb(145,152,159)")
         // alert(0);
     });
 
     init();
 });
 
-function loadData(data){
+function loadData(data) {
     // $(".j_fullPath").text(data._fullPath);
     // $(".j_compressedPath").text(data._compressedPath);
 
     var newResult = $(".j_resultPanel").eq(0).clone(true);
     $(newResult).find(".j_fullPath").text(data._fullPath);
     $(newResult).find(".j_compressedPath").text(data._compressedPath);
-    $(newResult).css("display","inline-block");
+    $(newResult).css("display", "inline-block");
 
     $(".j_resultContainer").append(newResult);
 }
 
-function saveData(data){
-    if(localData === null) {
+function saveData(data) {
+    if (localData === null) {
         localData = [];
     }
     localData[0] = data;
     window.localStorage.clear();
-    window.localStorage.setItem("data",JSON.stringify(localData));
+    window.localStorage.setItem("data", JSON.stringify(localData));
 }
 
-function init(){
+function init() {
     localData = JSON.parse(window.localStorage.getItem("data"));
 
-    for(var index in localData){
+    for (var index in localData) {
         loadData(localData[index]);
     }
 }
 
-function v(name)
-{
-    if(name.match("png") ||
+function v(name) {
+    if (name.match("png") ||
         name.match("jpg") ||
         name.match("JPG") ||
         name.match("PNG") ||
         name.match("jpeg") ||
-        name.match("JPEG")){
+        name.match("JPEG")) {
         return true;
     }
 
     return false;
-}
-
-function loading(){
-
 }

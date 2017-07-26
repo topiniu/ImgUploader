f2e0ca11cc088c01a5801cd14a328a3f9d1626a9
topiniu <topiniu@outlook.com>
2017-07-26 14:31:34 +0800

    修改逻辑-从服务端获取已上传图片列表 样式增加-copy完成后的弹窗提示

--- js/index.js
+++ js/index.js
@@ -2,10 +2,11 @@
  * Created by topiniu on 2017/7/3.
  */
 var localData = [];
-$(function(){
-    $("#j_checkBox").change(function(){
+$(function () {
+    getData();
+    $("#j_checkBox").change(function () {
         var file = $("#j_checkBox").val();
-        if(!v(file)){
+        if (!v(file)) {
             alert("Must be jpg or jpeg or png file");
             $("#j_checkBox").val(null);
             return;
@@ -22,90 +23,112 @@ $(function(){
             type: "POST",
             enctype: 'multipart/form-data',
             url: "http://23.106.158.25:8080/Project_BackSky_New/uploadimg",
+            // url: "http://localhost:8080/Project_BackSky_New/uploadimg",
             data: data,
             processData: false,
             contentType: false,
             cache: false,
             timeout: 600000,
-            success: function(data){
+            success: function (data) {
                 loadData(JSON.parse(data));
                 saveData(JSON.parse(data));
 
                 $(".j_loadingCover").fadeOut("slow");
             },
-            error: function(e){
+            error: function (e) {
                 alert("Something wrong");
             }
         });
     });
 
-    $(".j_path").on("click",function(){
+    $(".j_path").on("click", function () {
         // alert($(this));
         $(this).select()
+        $("#msgBox").fadeIn();
+        setTimeout(function () {
+            $("#msgBox").fadeOut();
+        }, 1500);
     });
 
-    new Clipboard('.btn',{
-        target: function(trigger){
+    new Clipboard('.btn', {
+        target: function (trigger) {
             return trigger;
         }
     });
 
     // new Clipboard('.btn');
-    $("#j_checkBox").on("mouseenter",function(){
-        $(".j_inputBox").css("backgroundColor","#BDC0BA")
+    $("#j_checkBox").on("mouseenter", function () {
+        $(".j_inputBox").css("backgroundColor", "#BDC0BA")
         // alert(0);
     });
-    $("#j_checkBox").on("mouseleave",function(){
-        $(".j_inputBox").css("backgroundColor","rgb(145,152,159)")
+    $("#j_checkBox").on("mouseleave", function () {
+        $(".j_inputBox").css("backgroundColor", "rgb(145,152,159)")
         // alert(0);
     });
 
     init();
 });
+function getData() {
 
-function loadData(data){
+    $.ajax({
+        type: "POST",
+        // url: "http://23.106.158.25:8080/Project_BackSky_New/uploadimg",
+        url: "http://localhost:8080/Project_BackSky_New/getImgList",
+        success: function (data) {
+            data = JSON.parse(data);
+            for (item in data) {
+
+                loadData(data[item]);
+                console.log(data[item]);
+            }
+        },
+        error: function (e) {
+            alert("Something wrong");
+        }
+    });
+}
+function loadData(data) {
     // $(".j_fullPath").text(data._fullPath);
     // $(".j_compressedPath").text(data._compressedPath);
 
     var newResult = $(".j_resultPanel").eq(0).clone(true);
     $(newResult).find(".j_fullPath").text(data._fullPath);
     $(newResult).find(".j_compressedPath").text(data._compressedPath);
-    $(newResult).css("display","inline-block");
+    $(newResult).css("display", "inline-block");
 
     $(".j_resultContainer").append(newResult);
 }
 
-function saveData(data){
-    if(localData === null) {
+function saveData(data) {
+    if (localData === null) {
         localData = [];
     }
     localData[0] = data;
     window.localStorage.clear();
-    window.localStorage.setItem("data",JSON.stringify(localData));
+    window.localStorage.setItem("data", JSON.stringify(localData));
 }
 
-function init(){
+function init() {
     localData = JSON.parse(window.localStorage.getItem("data"));
 
-    for(var index in localData){
+    for (var index in localData) {
         loadData(localData[index]);
     }
 }
 
-function v(name)
-{
-    if(name.match("png") ||
+function v(name) {
+    if (name.match("png") ||
         name.match("jpg") ||
         name.match("JPG") ||
         name.match("PNG") ||
         name.match("jpeg") ||
-        name.match("JPEG")){
+        name.match("JPEG")) {
         return true;
     }
 
     return false;
 }
 
-function loading(){
+function loading() {
 
 }

148d4f0baf46e99b8b5617ff233b7c9faf97cb24
topiniu <topiniu@outlook.com>
2017-07-27 16:56:55 +0800

    修改api

--- js/index.js
+++ js/index.js
@@ -21,8 +21,8 @@ $(function () {
         $.ajax({
             type: "POST",
             enctype: 'multipart/form-data',
-            // url: "http://23.106.158.25:8080/Project_BackSky_New/uploadimg",
-            url: "http://localhost:8080/Project_BackSky_New/uploadimg",
+            url: "http://23.106.158.25:8080/Project_BackSky_New/uploadimg",
+            // url: "http://localhost:8080/Project_BackSky_New/uploadimg",
             data: data,
             processData: false,
             contentType: false,
@@ -70,8 +70,8 @@ function getData() {
 
     $.ajax({
         type: "POST",
-        // url: "http://23.106.158.25:8080/Project_BackSky_New/getImgList",
-        url: "http://localhost:8080/Project_BackSky_New/getImgList",
+        url: "http://23.106.158.25:8080/Project_BackSky_New/getImgList",
+        // url: "http://localhost:8080/Project_BackSky_New/getImgList",
         success: function (data) {
             if (data === "") {
                 return;
